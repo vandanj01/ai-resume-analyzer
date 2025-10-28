b7651db79b49559f1a9c76efc31b5cdf33503808
vandanj01 <vandan.jain.01@gmail.com>
2025-10-28 15:20:29 +0530

    Authentication using Puter API

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,17 +2,29 @@ import type { Route } from "./+types/home";
 import Navbar from "~/components/Navbar";
 import {resumes} from "../../constants";
 import ResumeCard from "~/components/ResumeCard";
+import {usePuterStore} from "~/lib/puter";
+import {useEffect} from "react";
+import {useNavigate} from "react-router";
 
 
 export function meta({}: Route.MetaArgs) {
-  return [
+
+    return [
     { title: "Resumind" },
     { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <main className = "bg-[url('/images/bg-main.svg')] bg-cover">
+
+    const { auth } = usePuterStore();
+    const navigate = useNavigate();
+
+    useEffect( () => {
+        if(!auth.isAuthenticated) navigate('/auth?next=/');
+    }, [auth.isAuthenticated])
+
+    return <main className = "bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
     <section className="main-section">
         <div className="page-heading py-16">
